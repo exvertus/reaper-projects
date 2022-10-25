c0ad580e49b234e332644c7dc7c34e7017f446c8
tflanigan <tflanig1@gmail.com>
2022-10-25 15:50:24 -0500

    Reduce files tracked and don't use symlinks (Reaper update breaks symlinks)

--- make_links.py
+++ make_links.py
@@ -1,29 +1,44 @@
 """
-Use hardlinks to ensure Reaper config changes are reflected in repository from DAW edits.
+Keep settings and config synced with files in repo.
 """
 import logging
 import json
 import os
+import shutil
 import sys
 from pathlib import Path
 from subprocess import run
+from stat import S_IREAD, S_IWRITE
 
 logging.basicConfig(level=logging.DEBUG)
 
-def make_links(fp):
+def readonly_handler(function, path, execinfo):
     """
-    Creates hard symlinks from the config files in Reaper we care about into the repo.
+    Remove read-only permission from file.
+    """
+    path_obj = Path(path)
+    if execinfo[0] is PermissionError:
+        path_obj.chmod(S_IWRITE)
+        path_obj.unlink()
+
+def copy_readonlys(fp):
+    """
+    Creates read-only copies of reaper config files.
     """
     with open(Path(fp).absolute(), 'r') as f:
         config_map = json.load(f)
-    link_root = os.path.expandvars(config_map['repoPath'])
-    target_root = os.path.expandvars(config_map['reaperPath'])
-    for repo_f, reaper_f in config_map['fileMap'].items():
-        link = Path(link_root, repo_f)
-        target = Path(target_root, reaper_f)
-        logging.info(f"Linking {link} to {target}...")
-        link.hardlink_to(target)
-    logging.info(f"Linked {len(config_map['fileMap'])} files.")
+    copy_from = Path(os.path.expandvars(config_map['reaperPath']))
+    copy_to = Path(os.path.expandvars(config_map['repoPath']), 'configs')
+    if copy_to.exists():
+        shutil.rmtree(copy_to, onerror=readonly_handler)
+    copy_to.mkdir()
+    for name, repo_file in config_map['fileMap'].items():
+        source = Path(copy_from, repo_file)
+        destination = Path(copy_to, name)
+        shutil.copy(source, destination)
+        destination.chmod(S_IREAD)
+        logging.info(f"Copied {source} to {destination}...")
+    logging.info(f"Copied {len(config_map['fileMap'])} files.")
 
 if __name__ == '__main__':
     map_path = './reaperFiles.json'
@@ -31,4 +46,4 @@ if __name__ == '__main__':
         map_path = sys.argv[1]
     except IndexError:
         pass
-    make_links(map_path)
+    copy_readonlys(map_path)
